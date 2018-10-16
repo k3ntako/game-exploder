08084fcfaecbab343b379578b3102a6c5c828035
Kentaro Kaneki <k@Kentaros-MacBook-Pro.local>
2018-10-16 13:10:31 -0400

    Added sign-up functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
+require 'date'
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+  validates :birthday, presence: true
+  validates :username, presence: true
 end
--- db/migrate/20181015152149_devise_create_users.rb
+++ db/migrate/20181015152149_devise_create_users.rb
@@ -7,6 +7,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      ## User Basic Info
+      t.string :username, null: false
+      t.date :birthday, null: false
+      t.string :first_name
+      t.string :last_name
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -38,6 +44,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
+
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
