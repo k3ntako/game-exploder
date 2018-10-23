acf3da83d483be622a58d0d9d69024297278caa6
Richard Holway <rgholway@syr.edu>
2018-10-23 13:11:58 -0400

    Made syntax changes

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -4,8 +4,6 @@ protect_from_forgery unless: -> { request.format.json? }
   end
 
   def create
-    # data = JSON.parse(request.body.read)
-
     new_review = Review.create(title: review_params[:title], body: review_params[:body], score: review_params[:score], game_id: review_params[:game_id], user: current_user)
 
     render json: new_review, adapter: :json
