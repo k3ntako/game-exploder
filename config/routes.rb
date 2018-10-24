b38b4a191421ed79e409363c6308d492b1457959
Kentaro Kaneki <k@Kentaros-MacBook-Pro.local>
2018-10-24 14:31:14 -0400

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
     namespace :api do
       namespace :v1 do
         resources :games, only: [:index, :create, :show] do
-
           resources :reviews, only: [:new, :create, :index]
-
         end
     end
   end
 
+  root 'games#index'
+  get '*path', to: 'games#index'
 end
