1786d74de6354b42873e540dc3734ff0a059bb3b
Kentaro Kaneki <k@Kentaros-MacBook-Pro.local>
2018-10-24 11:50:11 -0400

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-    namespace :api do
-      namespace :v1 do
-        resources :games, only: [:index, :create, :show] do
-
-          resources :reviews, only: [:new, :create, :index]
-
-        end
+  namespace :api do
+    namespace :v1 do
+      resources :games, only: [:index, :create, :show] do
+        resources :reviews, only: [:new, :create, :index]
+      end
     end
   end
-
+  root 'games#index'
+  get '*path', to: 'games#index'
 end
